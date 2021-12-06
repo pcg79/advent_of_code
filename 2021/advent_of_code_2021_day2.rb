7667ddfa390a50733b9f2d5d236d9b2deb367644
Pat George <patrick.george@freeagent.com>
2021-12-06 11:00:41 +0000

    Finished day 2

--- 2021/advent_of_code_2021_day2.rb
+++ 2021/advent_of_code_2021_day2.rb
@@ -1,16 +1,18 @@
 def find_position(array)
   forward = 0
   depth = 0
+  aim = 0
   array.each do |command|
     direction, distance = command.split(" ")
     distance = distance.to_i
     case direction
     when "forward"
       forward += distance
+      depth += aim * distance
     when "down"
-      depth += distance
+      aim += distance
     when "up"
-      depth -= distance
+      aim -= distance
     end
   end
 
@@ -19,6 +21,6 @@ def find_position(array)
   forward * depth
 end
 
-data = File.open("/Users/patrickgeorge/dev/personal/leetcode/advent_of_code_2021_day2_input.txt").read.split("\n").map
+data = File.open("/Users/patrickgeorge/dev/personal/advent_of_code/2021/advent_of_code_2021_day2_input.txt").read.split("\n").map
 
 puts find_position(data)
