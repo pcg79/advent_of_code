eaccd8ca27a8e000b63451f862345609f83b5e75
Pat George <patrick.george@freeagent.com>
2021-12-14 10:26:01 +0000

    Day 14 part 2

--- 2021/advent_of_code_2021_day14.rb
+++ 2021/advent_of_code_2021_day14.rb
@@ -1,32 +1,37 @@
 def process_data(data, steps)
   template, rules = split_data(data)
-  # output = hashify(template)
+  start_char, end_char = template[0], template[-1]
+  output_hash = hashify(template)
+  letter_count = starting_count(template)
   steps.times do |i|
-    puts "*** i = #{i}"
-    output = process_step(output, rules)
+    output_hash, letter_count = process_step(output_hash, letter_count, rules)
   end
-  output
+  letter_count
 end
 
-# def hashify(string)
-#   hash = Hash.new(0)
-#
-#   0.upto(string.length - 2) do |i|
-#     pair = "#{string[i]}#{string[i+1]}"
-#     hash[pair] += 1
-#   end
-#   hash
-# end
-
-def quantity_score(input)
+def hashify(string)
   hash = Hash.new(0)
-  input.each_char do |char|
+
+  0.upto(string.length - 2) do |i|
+    pair = "#{string[i]}#{string[i+1]}"
+    hash[pair] += 1
+  end
+  hash
+end
+
+def starting_count(string)
+  hash = Hash.new(0)
+  string.each_char do |char|
     hash[char] += 1
   end
+  hash
+end
 
+def quantity_score(letter_count)
   max = 0
-  min = 2 ** 32 - 1
-  hash.each_pair do |char, count|
+  min = nil
+  letter_count.values.each do |count|
+    min = count unless min
     min = [min, count].min
     max = [count, max].max
   end
@@ -44,33 +49,67 @@ def split_data(data)
   return template, rules
 end
 
-def process_step(string, rules)
-  output = ""
-  0.upto(string.length - 2) do |i|
-    pair = "#{string[i]}#{string[i+1]}"
-    output << string[i] << rules[pair]
+{
+  "NN" => 1,
+  "NC" => 1,
+  "CB" => 1,
+}
+
+{
+  "N" => 2,
+  "C" => 1,
+  "B" => 1,
+}
+
+def process_step(pair_hash, letter_count, rules)
+  new_pair_hash = Hash.new(0)
+  pair_hash.each_pair do |pair, count|
+    new_char = rules[pair]
+    new_pair_one = "#{pair[0]}#{new_char}"
+    new_pair_two = "#{new_char}#{pair[1]}"
+    letter_count[new_char] += count
+    new_pair_hash[new_pair_one] += count
+    new_pair_hash[new_pair_two] += count
   end
-  output << string[-1]
-  output
+  return new_pair_hash, letter_count
 end
 
 data = File.open("/Users/patrickgeorge/dev/personal/advent_of_code/2021/advent_of_code_2021_day14_input.txt").read.split("\n")
 
-# pp quantity_score(process_data(data, 10))
+pp quantity_score(process_data(data, 10))
+
+pp quantity_score(process_data(data, 40))
 
 # ------- TESTS --------
 
 def test_process_data
   data = File.open("/Users/patrickgeorge/dev/personal/advent_of_code/2021/advent_of_code_2021_day14_test_input.txt").read.split("\n")
 
-  pp process_data(data, 1) == "NCNBCHB"
-  # pp process_data(data, 2) == "NBCCNBBBCBHCB"
+  count_hash1 = process_data(data, 1) #== "NCNBCHB"
+  pp count_hash1["N"] == 2 &&
+    count_hash1["C"] == 2 &&
+    count_hash1["B"] == 2 &&
+    count_hash1["H"] == 1
+
+
+  count_hash2 = process_data(data, 2) #== "NBCCNBBBCBHCB"
+  pp count_hash2["N"] == 2 &&
+    count_hash2["C"] == 4 &&
+    count_hash2["B"] == 6 &&
+    count_hash2["H"] == 1
+
   # pp process_data(data, 3) == "NBBBCNCCNBBNBNBBCHBHHBCHB"
   # pp process_data(data, 4) == "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
 
+  count_hash10 = process_data(data, 10)
+  pp count_hash10["N"] == 865 &&
+    count_hash10["C"] == 298 &&
+    count_hash10["B"] == 1749 &&
+    count_hash10["H"] == 161
+
   pp quantity_score(process_data(data, 10)) == 1588
 
-  # pp quantity_score(process_data(data, 40)) == 2188189693529
+  pp quantity_score(process_data(data, 40)) == 2188189693529
 end
 
 test_process_data
