d99501311b774a777d070d77b356f189126d9771
Pat George <patrick.george@freeagent.com>
2021-12-16 17:24:23 +0000

    Day 15 part 2

--- 2021/advent_of_code_2021_day15.rb
+++ 2021/advent_of_code_2021_day15.rb
@@ -8,6 +8,35 @@ def find_best_path_score(grid)
   best_path_hash[[grid.length - 1, grid[0].length - 1]]
 end
 
+def find_best_path_score_bigger_grid(grid)
+  bigger = build_bigger_grid(grid)
+  # print_grid(bigger)
+  best_path_hash, prev = find_best_path(bigger)
+  print_path(answer_grid, prev, bigger.length, bigger[0].length)
+
+  best_path_hash[[bigger.length - 1, bigger[0].length - 1]]
+end
+
+def build_bigger_grid(grid)
+  height = grid.length
+  width = grid[0].length
+
+  new_grid = Array.new(height * 5) { Array.new(width * 5) }
+
+  0.upto(4) do |dx|
+    0.upto(4) do |dy|
+      0.upto(grid.length - 1) do |x|
+        0.upto(grid[0].length - 1) do |y|
+          new_val = grid[x][y] + dx + dy
+          new_val = new_val - 9 if new_val > 9
+          new_grid[dx * height + x][dy * width + y] = new_val
+        end
+      end
+    end
+  end
+  new_grid
+end
+
 def find_best_path(grid)
   dist = Hash.new { Float::INFINITY }
   prev = Hash.new
@@ -51,7 +80,8 @@ end
 def print_grid(grid)
   grid.each do |line|
     output = line.map do |spot|
-      "%03d" % spot
+      # "%03d" % spot
+      spot
     end.join(",")
     puts output
   end
@@ -73,8 +103,10 @@ end
 
 data = File.open("/Users/patrickgeorge/dev/personal/advent_of_code/2021/advent_of_code_2021_day15_input.txt").read.split("\n").map { |line| line.split("").map(&:to_i) }
 # 404 too high
-pp find_best_path_score(data)
+# It was 403
+# pp find_best_path_score(data)
 
+pp find_best_path_score_bigger_grid(data)
 
 # ------- TESTS --------
 
@@ -84,7 +116,7 @@ def test_find_best_path
   pp find_best_path_score(data) == 40
 end
 
-test_find_best_path
+# test_find_best_path
 
 def test_find_best_path2
   data = File.open("/Users/patrickgeorge/dev/personal/advent_of_code/2021/advent_of_code_2021_day15_test_input_2.txt").read.split("\n").map { |line| line.split("").map(&:to_i) }
@@ -92,4 +124,12 @@ def test_find_best_path2
   pp find_best_path_score(data) == 20
 end
 
-test_find_best_path2
+# test_find_best_path2
+
+def test_find_best_path_bigger_grid
+  data = File.open("/Users/patrickgeorge/dev/personal/advent_of_code/2021/advent_of_code_2021_day15_test_input.txt").read.split("\n").map { |line| line.split("").map(&:to_i) }
+
+  pp find_best_path_score_bigger_grid(data) == 315
+end
+
+test_find_best_path_bigger_grid
